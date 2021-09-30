7d93265059987be2e8288624aacdccd41e5d44ee
supern0va1977 <85573435+supern0va1977@users.noreply.github.com>
2021-09-29 22:11:09 -0500

    lesson 2.5
    added  index details and expresscode

--- index.js
+++ index.js
@@ -1,49 +1,62 @@
 const express = require('express'),
-morgan = require('morgan');
+morgan = require('morgan'),
+bodyParser = require('body-parser'),
+uuid = require('uuid'),
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(morgan('common'));
 app.use(express.static('public'));
 
-let movies = [
+let moviesTop = [
   {
+    id:'1',
     title: 'Movie, index of 0',
     author: 'None 0'
   },
   {
+    id:'2',
     title: 'Movie, index of 1',
     author: 'None 1'
   },
   {
+    id:'3',
     title: 'Movie, index of 2',
     author: 'None 2'
   },
   {
+    id:'4',
     title: 'Movie, index of 3',
     author: 'None 3'
   },
   {
+    id:'5',
     title: 'Movie, index of 4',
     author: 'None 4'
   },
   {
+    id:'6',
     title: 'Movie, index of 5',
     author: 'None 5'
   },
   {
+    id:'7',
     title: 'Movie, index of 6',
     author: 'None 6'
   },
   {
+    id:'8',
     title: 'Movie, index of 7',
     author: 'None 7'
   },
   {
+    id:'9',
     title: 'Movie, index of 8',
     author: 'None 8'
   },
   {
+    id:'10',
     title: 'Movie, index of 9',
     author: 'None 9'
   },
@@ -51,16 +64,62 @@ let movies = [
 
 // GET requests
 app.get('/movies', (req, res) => {
-  res.json(movies);
+  res.json(moviesTop);
 });
-
+//message to users
 app.get('/', (req, res) => {
-  res.send('this is a test of upcomming movie application that is under development');
+  res.send('this is test #2 of upcomming movie application that is under development');
+});
+//gets the data about a movie
+app.get('/movies/:title', (req, res) => {
+  res.json(moviesTop.find((movie) => {
+    return movie.title === req.params.title
+  }));
+});
+//adds data for a new movie
+app.post('/movies', (req, res) => {
+  let newMovie = req.body;
+
+  if (!newMovie.title) {
+    const message = 'you are missing the name in request body';
+    res.status(400).send(message);
+  } else {
+    newMovie.id = uuid.v4();
+    moviesTop.push(newMovie);
+    res.ststus(201).send(newMovie);
+  }
 });
 
+//deletes a movie from the list by ID
+app.delete('/movies/:id', (req, res) => {
+  let movie = moviesTop.find((movie) => {
+    return movie.id === req.params.id
+  });
+
+  if (movie) {
+    moviesTop = moviesTop.filter((obj) => { return obj.id !== req.params.id });
+    res.status(201).send('movie wit the ID of ' + req.params.id + ' was deleted.');
+  }
+});
+
+//update the year of a movie by its title
+app.put('/movies/:title/:year', (req, res) => {
+  let movie = moviesTop.find((movie) => {
+    return movie.title = req.params.title
+  });
+
+  if (movie) {
+    movie.year = parseInt(req.params.year);
+    res.status(201).send(`Movie ${req.params.title} was assigned the year of ${req.params.year}.`);
+  } else {
+    res.status(404).send(`Movie wit the title ${req.params.title} was not found.`);
+  }
+});
+
+//error handling
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+  console.log(err.stack);
+  res.status(500).send('Oh NO! you broke it now!');
 });
 
 // listen for requests
