03cb29b23078cc238410293f8997710ba918f4d3
supern0va1977 <85573435+supern0va1977@users.noreply.github.com>
2021-11-17 19:52:25 -0600

    temp removal of movies auth

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get('/', (req, res) => {
 });
 
   //get list of all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }),(req, res) => {
+app.get('/movies', function (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
